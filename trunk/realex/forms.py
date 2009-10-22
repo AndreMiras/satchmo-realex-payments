68d983f4b33e85aff1402f07fcef6e3efa446230
andre.miras <andre.miras@6279478c-4224-11de-9cfd-b5a970547080>
2009-10-22 17:14:21 +0000

    cleaned up forms

--- trunk/realex/forms.py
+++ trunk/realex/forms.py
@@ -1,19 +1,12 @@
 from django import forms
 from satchmo.payment.forms import CreditPayShipForm
 from django.utils.translation import ugettext as _
-from satchmo.payment import signals
 
-from satchmo.contact.models import Contact, AddressBook
 
 class RealexCreditPayShipForm(CreditPayShipForm):
-    bill_street1 = forms.CharField(max_length=30, label=_('Street'))
-    bill_street2 = forms.CharField(max_length=30, required=False)
-    bill_city = forms.CharField(max_length=30, label=_('City'))
-    bill_postal_code = forms.CharField(max_length=10, label=_('ZIP code/Postcode'))
-    card_holder = forms.CharField(label=_("Card Holder Name"), max_length=60, required=False)
 
     def __init__(self, request, paymentmodule, *args, **kwargs):
-        CreditPayShipForm.__init__(self, request, paymentmodule, *args, **kwargs)
+        super(RealexCreditPayShipForm, self).__init__(request, paymentmodule, *args, **kwargs)
 
         # some Laser cards don't have a ccv
         self.fields['ccv'].required = False
